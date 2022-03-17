247729c1891519f48465ca8754eccda54744fc81
Erick Campos <kcire2477@gmail.com>
2022-03-17 13:46:36 -0600

    Version 2

--- js/app.js
+++ js/app.js
@@ -43,6 +43,7 @@ function busquedaImagen(){
 function mostrarImagenes(imagenes){
     limpiarHTML(resultado);
     imagenes.forEach(imagen => {
+        console.log(imagen);
         const { downloads, largeImageURL, likes, previewURL, views } = imagen;
         resultado.innerHTML += `
             <div class = "w-1/2 md:w-1/3 lg:w-1/4 p-3 mb-4">
